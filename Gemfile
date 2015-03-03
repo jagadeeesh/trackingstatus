ca21cf25923e7706cfb06a6646f3df5f8d7508a9
jagadeesh <k.jagadeesh123@gmail.com>
2015-03-03 16:40:21 +0530

    UI for home page

--- Gemfile
+++ Gemfile
@@ -30,7 +30,8 @@ gem 'spring',        group: :development
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 
-
+gem 'httparty'
+gem 'pry'
 
 gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   def  index
+     #res = HTTParty.get("https://indianrailways.p.mashape.com/index.php?pnr=4405520621", 
+     # :headers => { "X-Mashape-Key" => "QqLqo9Hx8Tmshbpj9A5oSZMTBlhTp1PUK1njsnutsVuCYxB3Dq","Accept" => "application/json" } )
+
     
   end
 end
